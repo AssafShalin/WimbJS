341032e04d725a11fab24b3e5335df916519f3a8
Assaf Shalin <Assaf.Shalin@Gmail.com>
2014-04-22 17:02:27 +0300

    Upload commit

--- ajax/lines.php
+++ ajax/lines.php
@@ -3,10 +3,10 @@ include 'functions.php';
 if(!isset($_GET['stationId']) || !is_numeric($_GET['stationId'])) die(json_encode(array('error'=>'invalid params')));
 $stationId = $_GET['stationId'];
 
-//$etaQuery = new LinesETAQuery($stationId);
-//$eta = $etaQuery->fetchLinesETA();
-//$stationInfo = $etaQuery->getStation();
-$stationInfo = getMockStationsData(array('443211'))[0];
-$eta = getMockLines('234234');
+$etaQuery = new LinesETAQuery($stationId);
+$eta = $etaQuery->fetchLinesETA();
+$stationInfo = $etaQuery->getStation();
+//$stationInfo = getMockStationsData(array('443211'))[0];
+//$eta = getMockLines('234234');
 $results = array('station' => $stationInfo, 'eta'=> $eta);
 echo json_encode($results);
--- ajax/stations.php
+++ ajax/stations.php
@@ -1,7 +1,9 @@
 <?php
 include 'functions.php';
 $stations = json_decode(file_get_contents('stations.json'));
-$stations = getMockStationsData($stations);
-//$stationsQuery = new StationsQuery($stations);
-//$stations = $stationsQuery->fetchStationsData();
+
+//$stations = getMockStationsData($stations);
+
+$stationsQuery = new StationsQuery($stations);
+$stations = $stationsQuery->fetchStationsData();
 echo json_encode($stations);
